233d9c928401ad2d0dcd8fab7d4c77b816ea98f1
Shina Mashiro <shinamashiro@ShinadeMacBook-Pro.local>
2018-11-16 21:53:13 +0800

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 600f;
+
+    private float movement = 0;
+    
+	// Update is called once per frame
+	void Update ()
+	{
+         movement = Input.GetAxisRaw("Horizontal");
+	}
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.back,movement*Time.fixedDeltaTime*moveSpeed);
+    }
+    static int i=0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag=="Finish")
+        {
+            Debug.Log(++i);
+            return;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
