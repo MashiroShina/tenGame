5126ba3bca49d7535639d4f6b3465bb3f6384d80
Shina Mashiro <shinamashiro@ShinadeMBP.lan>
2018-11-17 21:21:36 +0800

    upodate

--- Assets/Scripts/camera.cs
+++ Assets/Scripts/camera.cs
@@ -3,14 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class camera : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate(Vector3.forward*Time.deltaTime*30f);
+        //Debug.Log(transform .tag);
 	}
 }
